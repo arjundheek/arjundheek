ff8f15dadbe51587c66f4de2a7722a2ab6f956f1
Rohan Karan <rohankaran001@gmail.com>
2023-07-14 16:57:34 +0530

    Update admin.py

--- ArjunVaas/admin.py
+++ ArjunVaas/admin.py
@@ -3,12 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display=('name','category', 'gender')
+    list_display = ("name", "category", "gender")
+
 
 admin.site.register(Brand)
 admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductDuplicate)
 admin.site.register(Tryon)
 admin.site.register(Banner)
 admin.site.register(Document)
