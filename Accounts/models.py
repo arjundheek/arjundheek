7028123d376e3a3e33716e72a763b05eccbc8b07
Rohan Karan <rohankaran001@gmail.com>
2023-07-21 16:36:26 +0530

    Update models.py

--- Accounts/models.py
+++ Accounts/models.py
@@ -15,6 +15,7 @@ class Profile(models.Model):
     skin = models.CharField(max_length=15, default='', blank=True)
     height = models.FloatField(max_length=2, default=0, blank=True)
     size = models.CharField(max_length=3, default='', blank=True)
+    email = models.EmailField(blank=True)
 
     def __str__(self):
         return str(f"{self.user.username} - {self.name}")
